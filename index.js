a907c9abcc2381eed190850a9d682ba54eec4aac
Quynh <qpham2594@gmail.com>
2023-06-15 11:52:32 -0400

    issues fixed with updated comments

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-var toDoForm = document.querySelector('form'); 
-var listItemsArray = []
+
+var toDoForm = document.getElementById('add-todo'); 
+var todoList = document.getElementById('todo-list');             // Creating variable for <ul> with that ID  
+var todoConst = []
+
+// ** The main issue for not passing 9th and 10th tests of adding multiple todos and removing any todo item was in the name of the <ul> variable; <ul> variable name was previously 'bulletPoints', and it became 'todoList' **
 
 toDoForm.onsubmit = function(e) {
     e.preventDefault()                                     //prevent default action from happening 
-    var taskInput = document.querySelector('input');        //variable for input 
+    
+    var taskInput = document.querySelector('#add-todo input');        //variable for input 
     var task = taskInput.value.trim();                      // variable for the value of the input
-    listItemsArray.push(task);                              // push the array to the value of the input
+    todoConst.push(task);                              // push the array to the value of the input
    
     if (task === '') {                              // if value of input is empty, then nothing will add // prevent adding blanks
         return;
     }
-
-    var bulletPoints = document.getElementById('todo-list');             // Creating variable for  <ul> with that ID 
 
     // Creating elements for HTML without modifying HTML
 
@@ -21,39 +24,27 @@ toDoForm.onsubmit = function(e) {
 
     // Connecting the created HTML elements - from most outer to most inner: <ul> connecting to <li>, then <li> connecting to <button>, and then <button> connecting to the textnode where the value is being entered in
 
-    bulletPoints.appendChild(li);
+    todoList.appendChild(li);
     li.appendChild(button);
     button.appendChild(taskText);
 
-    
     // Using mouse click counts, it can detects the number of mouse clicks on each of the item listed
 
     var mouseClickCount = 0;
     button.addEventListener('click', function() {                       // adding event listener to let the program knows what do to with the button
-      mouseClickCount++;
-      console.log(listItemsArray)
+        mouseClickCount++;
+
       if (mouseClickCount === 1) {                                      // with 1 click, the item will be crossed-out
         button.style.textDecoration = 'line-through';
-      } else if (mouseClickCount === 2) {                                 // with the second click, the <li> button will be remove so the item will be taken off the list
-        var index = listItemsArray.indexOf(task);                        // index = the specific index of a task from the array 
-        if (index !== -1) {                                              // if the index is not on the list
-            listItemsArray.splice(index,1);                             // then the index will be removed (splice) from the array                                
-        }
-        li.remove();                                                    // li button is removed
+      
+    } else if (mouseClickCount === 2) {                                 // with the second click, the <li> button will be remove so the item will be taken off the list
+            var index =  todoConst.indexOf(task);                        // index = the specific index of a task from the array 
+            if (index !== -1) {                                              // if the index is not on the list
+            todoConst.splice(index,1);    }                         // then the index will be removed (splice) from the array                                
+        
+            li.remove();                                            // li button is removed                                                 
       }
     });
-
-console.log(bulletPoints)
-taskInput.value = ''                                                // to clear out the value or item being entered after it is added, a reset 
-}
-
-
-
-
-
-
-//Isues need to be fixed:
-    // Add multiple todos 
-    // Remove todos from middle of the list
-
-
+   
+    taskInput.value = ''                                                // to clear out the value or item being entered after it is added, a reset 
+} 
